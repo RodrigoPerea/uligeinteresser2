284855706b30a5d33ed099824a7ed6ce0608c508
Christian Rodrigo <chri65mb@stud.kea.dk>
2023-02-21 00:10:11 +0100

    Database virker ish, sortering virker ikke

--- js/cocktailliste.js
+++ js/cocktailliste.js
@@ -2,17 +2,22 @@ const urlParams = new URLSearchParams(window.location.search);
 let cat = urlParams.get("spiritus_sortering");
 let productCategorySelector = document.getElementById("categorySelector");
 const url = "https://drinks-7edc.restdb.io/rest/cocktails"; //endpoint, gemmer i variablen url
-
+const main = document.querySelector("main");
 //objekt, de rindeholder et andet objekt, så den ved den har adgang til databasen
 //API nøgel blir pakket ind i mit objekt så jeg kan sende den med fetch
 const options = {
   headers: {
-    "x-apikey": "a4b544f910bd22d3e426583aa3c5f164363e7",
+    "x-apikey": "63f36afc478852088da684b5",
   },
 };
 
+const catUrl = `https://drinks-7edc.restdb.io/rest/cocktails?q={"$distinct":"spiritus_sortering"}&sort=spiritus_sortering`; //endpoint, gemmer i variablen url
+
+//objekt, de rindeholder et andet objekt, så den ved den har adgang til databasen
+//API nøgel blir pakket ind i mit objekt så jeg kan sende den med fetch
+
 //filter
-productCategorySelector.onchange = () => {
+/* productCategorySelector.onchange = () => {
   location.reload();
 };
 if (cat !== null) {
@@ -20,22 +25,41 @@ if (cat !== null) {
   productCategorySelector.parentElement.style.display = "none";
 } else if (productCategorySelector.value !== "null") {
   url += `&category=${productCategorySelector.value}`;
-}
+} */
 
 async function getData() {
   const response = await fetch(url, options);
   const json = await response.json();
-  console.log(response);
+  console.log("json: " + json);
   vis(json);
-  /*   response.forEach(vis); //loope */
 }
-const main = document.querySelector("main");
-const template = document.querySelector("listTemplate");
+async function getSortering() {
+  console.log("getSotering: ");
+  const response = await fetch(catUrl, options);
+  const json = await response.json();
+  console.log("json: " + json);
+  visSortering(json);
+}
+
+function visSortering(json) {
+  console.log("visSortering: ");
+  json.forEach((spiritus) => {
+    const template = document.querySelector("#sortering").content;
+    const clone = template.cloneNode(true);
+    console.log("sortering: " + spiritus);
+    clone.querySelector("option").value = spiritus;
+    clone.querySelector("option").textcontent = spiritus;
+    document.querySelector("#categorySelector").appendChild(clone);
+  });
+}
+
 function vis(json) {
   json.forEach((cocktail) => {
+    const template = document.querySelector("#listTemplate").content;
     //looper igennem jason, og for hver person kloner jeg mit skabelon
     //og sætter det rigtige indhold ind
     const clone = template.cloneNode(true);
+    /* console.log("clone: " + template.innerHTML); */
     clone.querySelector(".billede").src = "pic/" + cocktail.pictur;
     clone.querySelector(".drink_navn").textcontent = cocktail.navn;
     clone.querySelector(".land").textcontent = cocktail.land;
