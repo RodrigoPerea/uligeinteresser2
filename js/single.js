e4845739cc45740abb8cadb4bb3db7b8c66c3404
Christian Rodrigo <chri65mb@stud.kea.dk>
2023-02-22 23:13:04 +0100

    tilføjet link

--- js/single.js
+++ js/single.js
@@ -9,35 +9,41 @@
 console.log("sidenVises");
 
 const urlParams = new URLSearchParams(window.location.search);
-const url = `https://kea-alt-del.dk/t7/api/products?limit=20&category=${cat}`;
+const url = `https://drinks-7edc.restdb.io/rest/cocktails`;
+
+const options = {
+  headers: {
+    "x-apikey": "63ea50de478852088da681a9",
+  },
+};
 
 async function hentData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    data.forEach(visProdukt);
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  data.forEach(visProdukt);
 }
 
 // document.querySelector(".").textContent = ;
 
 function visProdukt(produkt) {
-    console.log(produkt);
-    const template = document.querySelector("#templateOne").content;
-    const id = produkt.id;
-    const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
-    const copy = template.cloneNode(true);
-    copy.querySelector(".navn").textContent = produkt.navn;
-    copy.querySelector(".land").textContent = produkt.land;
-    copy.querySelector(".smag1").textContent = produkt.smag_1;
-    copy.querySelector(".smag2").textContent = produkt.smag_2;
-    copy.querySelector(".oc_1").textContent = produkt.oc_1;
-    copy.querySelector(".oc_2").textContent = produkt.oc_2;
-    copy.querySelector(".opskrift").textContent = produkt.opskrift;
-    copy.querySelector(".tag").textContent = produkt.tag_line;
-    copy.querySelector("pictur").src = imagePath;
-    copy.querySelector("a").href = "product.html?id=" + produkt.id;
- 
-    document.querySelector("main").appendChild(copy);
+  console.log(produkt);
+  const template = document.querySelector("#templateOne").content;
+  const id = produkt.id;
+  const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".navn").textContent = produkt.navn;
+  copy.querySelector(".land").textContent = produkt.land;
+  copy.querySelector(".smag1").textContent = produkt.smag_1;
+  copy.querySelector(".smag2").textContent = produkt.smag_2;
+  copy.querySelector(".oc_1").textContent = produkt.oc_1;
+  copy.querySelector(".oc_2").textContent = produkt.oc_2;
+  copy.querySelector(".opskrift").textContent = produkt.opskrift;
+  copy.querySelector(".tag").textContent = produkt.tag_line;
+  copy.querySelector("pictur").src = imagePath;
+  copy.querySelector("a").href = "product.html?id=" + produkt.id;
+
+  document.querySelector("main").appendChild(copy);
 }
 
 hentData();
