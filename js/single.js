5970f539f7df6c685ee4dfdf41f01ce553c623fc
Christian Rodrigo <chri65mb@stud.kea.dk>
2023-02-23 14:12:41 +0100

    Har porøvet at rette sidne. udne held. burger fix

--- js/single.js
+++ js/single.js
@@ -6,10 +6,55 @@
 //   navBarMenu.classList.toggle("active");
 // });
 
-console.log("sidenVises");
+// console.log("sidenVises");
+
+// const urlParams = new URLSearchParams(window.location.search);
+// const url = `https://drinks-7edc.restdb.io/rest/cocktails`;
+
+// const options = {
+//   headers: {
+//     "x-apikey": "63ea50de478852088da681a9",
+//   },
+// };
+
+// async function hentData() {
+//   const response = await fetch(itemDataFromId);
+//   const data = await response.json();
+//   console.log(data);
+//   data.forEach(visProdukt);
+// }
+
+// document.querySelector(".").textContent = ;
+
+// function visProdukt(produkt) {
+//   console.log(produkt);
+//   const template = document.querySelector("#templateOne").content;
+//   const id = produkt.id;
+//   const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+//   const copy = template.cloneNode(true);
+//   copy.querySelector(".navn").textContent = produkt.navn;
+//   copy.querySelector(".land").textContent = produkt.land;
+//   copy.querySelector(".smag1").textContent = produkt.smag_1;
+//   copy.querySelector(".smag2").textContent = produkt.smag_2;
+//   copy.querySelector(".oc_1").textContent = produkt.oc_1;
+//   copy.querySelector(".oc_2").textContent = produkt.oc_2;
+//   copy.querySelector(".opskrift").textContent = produkt.opskrift;
+//   copy.querySelector(".tag").textContent = produkt.tag_line;
+//   copy.querySelector("pictur").src = imagePath;
+//   copy.querySelector("a").href = "product.html?id=" + produkt.id;
+
+//   document.querySelector("main").appendChild(copy);
+// }
+
+// hentData();
+
+//
+
+//rodrigos omskrivning
 
 const urlParams = new URLSearchParams(window.location.search);
-const url = `https://drinks-7edc.restdb.io/rest/cocktails`;
+const id = urlParams.get("id");
+const itemDataFromId = `https://drinks-7edc.restdb.io/rest/cocktails/${id}`;
 
 const options = {
   headers: {
@@ -18,20 +63,14 @@ const options = {
 };
 
 async function hentData() {
-  const response = await fetch(url);
+  const response = await fetch(itemDataFromId);
   const data = await response.json();
-  console.log(data);
-  data.forEach(visProdukt);
+  console.log("data fra id" + data);
+  visProdukt(data);
 }
 
-// document.querySelector(".").textContent = ;
-
 function visProdukt(produkt) {
-  console.log(produkt);
-  const template = document.querySelector("#templateOne").content;
-  const id = produkt.id;
-  const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
-  const copy = template.cloneNode(true);
+  clone.querySelector(".billede").src = "pic/ " + produkt.pictur;
   copy.querySelector(".navn").textContent = produkt.navn;
   copy.querySelector(".land").textContent = produkt.land;
   copy.querySelector(".smag1").textContent = produkt.smag_1;
@@ -40,7 +79,7 @@ function visProdukt(produkt) {
   copy.querySelector(".oc_2").textContent = produkt.oc_2;
   copy.querySelector(".opskrift").textContent = produkt.opskrift;
   copy.querySelector(".tag").textContent = produkt.tag_line;
-  copy.querySelector("pictur").src = imagePath;
+
   copy.querySelector("a").href = "product.html?id=" + produkt.id;
 
   document.querySelector("main").appendChild(copy);
